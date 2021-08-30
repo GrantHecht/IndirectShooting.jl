46eaedaf6c20aeb4cc2fc9587e47c51055a3ae48
Documenter.jl <documenter@juliadocs.github.io>
2021-08-30 13:09:56 +0000

    build based on 251ba78

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = IndirectShooting","category":"page"},{"location":"#IndirectShooting","page":"Home","title":"IndirectShooting","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for IndirectShooting.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [IndirectShooting]","category":"page"}]
+}
