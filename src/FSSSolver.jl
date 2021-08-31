6e869a054fa5f28d94a9ae926e28d26cb913be5d
Grant Hecht <granthec@buffalo.edu>
2021-08-31 14:23:26 -0400

    Updated FSSSolver nonlinear equations to use ShootingDataManager methods.

--- src/FSSSolver.jl
+++ src/FSSSolver.jl
@@ -53,6 +53,11 @@ function fssNLEqFunc!(F::Union{Nothing,AbstractArray}, J::Union{Nothing,Abstract
                       λ::AbstractArray, bvpFuncNoSTM, bvpFuncWSTM, 
                       sdm::ShootingDataManager)
 
+    # Get data from ShootingDataManager
+    z0 = GetInputVec(sdm)
+    zf = GetOutputVec(sdm)
+    fc = GetFinConditions(sdm)
+
     # Fill J with zeros if necessary
     if !(J === nothing)
         fill!(J, 0.0)
@@ -66,36 +71,36 @@ function fssNLEqFunc!(F::Union{Nothing,AbstractArray}, J::Union{Nothing,Abstract
     # Evaluate boundary value problem and fill data arrays
     if J === nothing && F !== nothing
         # Initialize full initial condition vector w/o STM
-        sdm.inVec[m+1:n] .= λ
+        z0[m+1:n] .= λ
 
         # Evaluate boundary value function
-        @views sdm.outVec[1:n] .= bvpFuncNoSTM(sdm.inVec[1:n])
+        @views zf[1:n] .= bvpFuncNoSTM(z0[1:n])
 
         # Fill F (This is not generalized!)
-        @views F[1:6] .= sdm.outVec[1:6] .- sdm.fcs[1:6]
-        F[7] = sdm.outVec[14] - sdm.fcs[7]
+        @views F[1:6] .= zf[1:6] .- fc[1:6]
+        F[7] = zf[14] - fc[7]
     elseif J !== nothing
         # Initialize full initial condition vector w/ STM
-        sdm.inVec[m+1:n] .= λ
+        z0[m+1:n] .= λ
 
         # !!! THIS BLOCK OF CODE MAY NOT BE NECESSARY
-        sdm.inVec[n+1:end] .= 0.0
-        for i in 1:n
-            sdm.inVec[n + (i-1)*n + i] = 1.0
-        end
+        #sdm.inVec[n+1:end] .= 0.0
+        #for i in 1:n
+        #    sdm.inVec[n + (i-1)*n + i] = 1.0
+        #end
         # !!! END BLOCK
 
         # Evaluate boundary value function
-        sdm.outVec .= bvpFuncWSTM(sdm.inVec)
+        zf .= bvpFuncWSTM(z0)
 
         # Fill F (This is not generalized!)
         if F !== nothing 
-            @views F[1:6] .= sdm.outVec[1:6] .- sdm.fcs[1:6]
-            F[7] = sdm.outVec[14] - sdm.fcs[7]
+            @views F[1:6] .= zf[1:6] .- fc[1:6]
+            F[7] = zf[14] - fc[7]
         end
 
         # Fill J
-        @views STM = reshape(sdm.outVec[n+1:end], (n,n))
+        @views STM = reshape(zf[n+1:end], (n,n))
         @views J .= STM[[1,2,3,4,5,6,14], 8:14] # Not generalized!
     end
 
